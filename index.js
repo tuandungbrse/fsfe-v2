766b1503376479e9c7f763b442dd5cbeebf4bed4
DungTNT <tuandungbrse@gmail.com>
2021-01-27 16:41:46 +0000

    customer headers

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const port = 3000;
 
 app.get('/',(req,res) => {
 
-res.send('Hello World fron ExpressJS');
+res.send('Hello World from Express server.');
+});
+
+app.get('demo',(req,res) => {
+
+	res.set('x-full-stack','for-life');
+	res.status(418);
+	res.send('customer headers');
+
+
 });
 
 app.listen(port,() => {console.log('app is running on port ',port)});
